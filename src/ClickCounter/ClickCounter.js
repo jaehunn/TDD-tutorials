597b3592f09bb64dd221e58fe145352dd116e5ff
jaehunn <qkdwogns98@gmail.com>
2020-08-25 22:44:31 +0900

    progress...

--- src/ClickCounter/ClickCounter.js
+++ src/ClickCounter/ClickCounter.js
@@ -6,10 +6,15 @@ var App = App || {};
  */
 App.Counter = (_data) => {
   const data = _data;
+
+  // initial value
   data.value = data.value || 0;
+  data.operator = data.operator || (() => data.value);
 
   const appCounter = {
     getValue: () => data.value,
+    increaseValue: () => data.value + 1,
+    getOperatedValue: () => data.operator(data.value),
   };
 
   return appCounter;
--- src/ClickCounter/spec/ClickCounter.spec.js
+++ src/ClickCounter/spec/ClickCounter.spec.js
@@ -4,15 +4,32 @@ describe("AppCounter", () => {
 
   beforeEach(() => {
     // state
-    data = { value: 0 };
+    data = { value: 2, operator: (value) => value * 2 };
 
     // object
     appCounter = App.Counter(data);
   });
 
+  // 1. return value
   describe("getValue()", () => {
     it("Return value", () => {
       expect(appCounter.getValue()).toBe(data.value);
+    });
+  });
+
+  // 2. increase value
+  describe("increaseValue()", () => {
+    it("Increase value", () => {
+      expect(appCounter.increaseValue()).toBe(data.value + 1);
+    });
+  });
+
+  // 3. return operated value
+  describe("getOperatedValue()", () => {
+    it("Calculated by operator", () => {
+      const result = data.operator(data.value);
+
+      expect(appCounter.getOperatedValue()).toBe(result);
     });
   });
 });
